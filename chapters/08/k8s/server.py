80434a2dfd8803a7c1420dba5fdadf7a3c0ab6b6
Adam Hopkins <adam@amhopkins.com>
2021-11-29 09:27:57 +0200

    Add chapter 8 code

---
+++ chapters/08/k8s/server.py
@@ -0,0 +1,16 @@
+from sanic import Sanic, text
+from sanic.log import logger
+
+app = Sanic(__name__)
+app.config.REAL_IP_HEADER = "x-real-ip"
+
+
+@app.get("/")
+async def handler(request):
+    logger.info(request.headers)
+    return text(f"Hello from {request.remote_addr}")
+
+
+@app.get("/healthz")
+async def healthz(request):
+    return text("OK")
